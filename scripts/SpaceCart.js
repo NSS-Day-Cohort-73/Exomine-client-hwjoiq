993d217efd5e9499e86c4e8e9aedf94f49deee43
Matthew Clonch <matthewrclonch@gmail.com>
2024-10-16 13:36:40 -0500

    defined space cart, updated transient state, created change event for facilityminerals, and invoked renderspacecart inside

--- scripts/SpaceCart.js
+++ scripts/SpaceCart.js
@@ -3,13 +3,6 @@ import { purchaseMineral } from "./TransientState.js"
 export const renderSpaceCart = (selectedMineral, selectedFacility) => {
     const cartContents = document.getElementById("cart__contents")
 
-    document.addEventListener("click", makePurchase)
-
     cartContents.innerHTML = `1 ton of ${selectedMineral} from ${selectedFacility}`
 }
 
-const makePurchase = (clickEvent) => {
-    if (clickEvent.target.id === "purchase__btn") {
-        purchaseMineral()
-    }
-} 
